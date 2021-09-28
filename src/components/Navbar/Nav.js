a364e5c329ecde76e7c4fee1fee5a64b94d68fd8
joyaljosescaria <joyaljosescaria1@gmail.com>
2021-09-28 15:39:45 +0530

    Changes Added

--- src/components/Navbar/Nav.js
+++ src/components/Navbar/Nav.js
@@ -6,28 +6,28 @@ export default function Nav() {
     return (
         <div>
             <ul>
-                 <a target="blank" href="https://facebook.com"><div className="rotate fb">  
+                 <a  href="https://facebook.com"><div className="rotate fb">  
                     <li><i className="fa fa-facebook"></i></li>
                 </div></a>
-                 <a target="blank" href="https://twitter.com"><div className="rotate twt">
+                 <a  href="https://twitter.com"><div className="rotate twt">
                     <li><i className="fa fa-twitter"></i></li>
                 </div></a>
-                 <a target="blank" href="https://instagram.com"><div className="rotate insta">
+                 <a  href="https://instagram.com"><div className="rotate insta">
                     <li><i className="fa fa-instagram"></i></li>
                 </div></a>
-                 <a target="blank" href="https://linkedin.com"><div className="rotate in">
+                 <a  href="https://linkedin.com"><div className="rotate in">
                     <li><i className="fa fa-linkedin"></i></li>
                 </div></a>
-                 <a target="blank" href="https://trello.com"><div className="rotate tr">
+                 <a  href="https://trello.com"><div className="rotate tr">
                     <li><i className="fa fa-trello"></i></li>
                 </div></a>
-                 <a target="blank" href="https://github.com"><div className="rotate gt">
+                 <a  href="https://github.com"><div className="rotate gt">
                     <li><i className="fa fa-github"></i></li>
                 </div></a>
-                 <a target="blank" href="https://youtube.com"><div className="rotate yt">
+                 <a  href="https://youtube.com"><div className="rotate yt">
                     <li><i className="fa fa-youtube"></i></li>
                 </div></a>
-                 <a target="blank" href="https://stackoverflow.com"><div className="rotate st">
+                 <a  href="https://stackoverflow.com"><div className="rotate st">
                     <li><i className="fa fa-stack-overflow"></i></li>
                 </div></a>
             </ul>
